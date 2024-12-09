db1cab95c30a7131f9d98acc27e4a22f60907742
Emma Frith <emma.frith@education.gov.uk>
2024-12-09 12:02:44 +0000

    start pulling through data

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -26,8 +26,59 @@ module.exports = {
     "hasLimitedSpeed40": true,
     "travelInConvoy": false,
     "substantiallyModified": true,
+  },
+  {
+    "id": 38490,
+    "fullName": "Emma Frith",
+    "address": {
+      "firstLine": "22 shoredith road",
+      "town": "London",
+      "postCode": "L22 4FW"
+    },
+    "vehicleYear": 2023,
+    "vehicleReg": "LR24 DVC",
+    "vehicleDescription": "Black Toyota",
+    "vehicleLength": {
+      "isLonger": true,
+      "meters": 19.3
+    },
+    "vehicleAttributes": {
+      "livingAccomodation": true,
+      "transportForAnimals" : false,
+      "hasGenerator" : true,
+      "hasRide": true
+    },
+    "totalWeight": 4,
+    "hasLimitedSpeed40": true,
+    "travelInConvoy": false,
+    "substantiallyModified": true,
+  },
+  {
+    "id": 84920,
+    "fullName": "Emma Frith",
+    "address": {
+      "firstLine": "22 shoredith road",
+      "town": "London",
+      "postCode": "L22 4FW"
+    },
+    "vehicleYear": 2023,
+    "vehicleReg": "LR24 DVC",
+    "vehicleDescription": "Black Toyota",
+    "vehicleLength": {
+      "isLonger": true,
+      "meters": 19.3
+    },
+    "vehicleAttributes": {
+      "livingAccomodation": true,
+      "transportForAnimals" : false,
+      "hasGenerator" : true,
+      "hasRide": true
+    },
+    "totalWeight": 4,
+    "hasLimitedSpeed40": true,
+    "travelInConvoy": false,
+    "substantiallyModified": true,
   }
-
   ]
 
 }
