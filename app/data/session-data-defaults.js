0d3953987f68b263bee472116842aaf436b4d807
aabdi18 <150015326+aabdi18@users.noreply.github.com>
2024-12-09 12:07:14 +0000

    added more IDs

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -78,6 +78,162 @@ module.exports = {
     "hasLimitedSpeed40": true,
     "travelInConvoy": false,
     "substantiallyModified": true,
+  },
+  {
+    "id": 12892,
+    "fullName": "Emma Frith",
+    "address": {
+      "firstLine": "22 shoredith road",
+      "town": "London",
+      "postCode": "L22 4FW"
+    },
+    "vehicleYear": 2023,
+    "vehicleReg": "LR24 DVC",
+    "vehicleDescription": "Black Toyota",
+    "vehicleLength": {
+      "isLonger": true,
+      "meters": 19.3
+    },
+    "vehicleAttributes": {
+      "livingAccomodation": true,
+      "transportForAnimals" : false,
+      "hasGenerator" : true,
+      "hasRide": true
+    },
+    "totalWeight": 4,
+    "hasLimitedSpeed40": true,
+    "travelInConvoy": false,
+    "substantiallyModified": true,
+  },
+  {
+    "id": 38790,
+    "fullName": "Emma Frith",
+    "address": {
+      "firstLine": "22 shoredith road",
+      "town": "London",
+      "postCode": "L22 4FW"
+    },
+    "vehicleYear": 2023,
+    "vehicleReg": "LR24 DVC",
+    "vehicleDescription": "Black Toyota",
+    "vehicleLength": {
+      "isLonger": true,
+      "meters": 19.3
+    },
+    "vehicleAttributes": {
+      "livingAccomodation": true,
+      "transportForAnimals" : false,
+      "hasGenerator" : true,
+      "hasRide": true
+    },
+    "totalWeight": 4,
+    "hasLimitedSpeed40": true,
+    "travelInConvoy": false,
+    "substantiallyModified": true,
+  },
+  {
+    "id": 287320,
+    "fullName": "Emma Frith",
+    "address": {
+      "firstLine": "22 shoredith road",
+      "town": "London",
+      "postCode": "L22 4FW"
+    },
+    "vehicleYear": 2023,
+    "vehicleReg": "LR24 DVC",
+    "vehicleDescription": "Black Toyota",
+    "vehicleLength": {
+      "isLonger": true,
+      "meters": 19.3
+    },
+    "vehicleAttributes": {
+      "livingAccomodation": true,
+      "transportForAnimals" : false,
+      "hasGenerator" : true,
+      "hasRide": true
+    },
+    "totalWeight": 4,
+    "hasLimitedSpeed40": true,
+    "travelInConvoy": false,
+    "substantiallyModified": true,
+  },
+  {
+    "id": 12892,
+    "fullName": "Emma Frith",
+    "address": {
+      "firstLine": "22 shoredith road",
+      "town": "London",
+      "postCode": "L22 4FW"
+    },
+    "vehicleYear": 2023,
+    "vehicleReg": "LR24 DVC",
+    "vehicleDescription": "Black Toyota",
+    "vehicleLength": {
+      "isLonger": true,
+      "meters": 19.3
+    },
+    "vehicleAttributes": {
+      "livingAccomodation": true,
+      "transportForAnimals" : false,
+      "hasGenerator" : true,
+      "hasRide": true
+    },
+    "totalWeight": 4,
+    "hasLimitedSpeed40": true,
+    "travelInConvoy": false,
+    "substantiallyModified": true,
+  },
+  {
+    "id": 56490,
+    "fullName": "Emma Frith",
+    "address": {
+      "firstLine": "22 shoredith road",
+      "town": "London",
+      "postCode": "L22 4FW"
+    },
+    "vehicleYear": 2023,
+    "vehicleReg": "LR24 DVC",
+    "vehicleDescription": "Black Toyota",
+    "vehicleLength": {
+      "isLonger": true,
+      "meters": 19.3
+    },
+    "vehicleAttributes": {
+      "livingAccomodation": true,
+      "transportForAnimals" : false,
+      "hasGenerator" : true,
+      "hasRide": true
+    },
+    "totalWeight": 4,
+    "hasLimitedSpeed40": true,
+    "travelInConvoy": false,
+    "substantiallyModified": true,
+  },
+  {
+    "id": 12320,
+    "fullName": "Emma Frith",
+    "address": {
+      "firstLine": "22 shoredith road",
+      "town": "London",
+      "postCode": "L22 4FW"
+    },
+    "vehicleYear": 2023,
+    "vehicleReg": "LR24 DVC",
+    "vehicleDescription": "Black Toyota",
+    "vehicleLength": {
+      "isLonger": true,
+      "meters": 19.3
+    },
+    "vehicleAttributes": {
+      "livingAccomodation": true,
+      "transportForAnimals" : false,
+      "hasGenerator" : true,
+      "hasRide": true
+    },
+    "totalWeight": 4,
+    "hasLimitedSpeed40": true,
+    "travelInConvoy": false,
+    "substantiallyModified": true,
   }
   ]
 
